ceebf6da9abcf539676bf0738f86174cd367a62a
chrisco210 <chrisrachlinski@gmail.com>
2017-09-28 23:18:55 -0400

    Created functions to convert int to byte array and back

--- src/cf/rachlinski/autoEval/storage/export/project/Header.java
+++ src/cf/rachlinski/autoEval/storage/export/project/Header.java
@@ -3,50 +3,26 @@ package cf.rachlinski.autoEval.storage.export.project;
 /**
  * A header to include in an AutoEvalProject
  */
-class Header {
+class Header
+{
 	/**
 	 * The bytes in the header
 	 */
 	private byte[] header;
 
 	/**
-	 * Create a new header.  Note the bytes in the header cannot be changed
-	 *
-	 * @param headerBytes the bytes to include in the header. This must be less than 4 bytes
+	 * Size of {@code int} in java
 	 */
-	Header(byte[] headerBytes) {
-		//Check to make sure the header isn't more than 4 bytes
-		if (headerBytes.length > 4)
-			throw new IllegalArgumentException();
-
-		byte[] tempBytes = new byte[8];
-
-		for (int i = 0; i < 4; i++)
-			tempBytes[i] = headerBytes[i];
-		for (int i = 4; i < 8; i++)
-			tempBytes[i] = 0;
-
-		this.header = tempBytes;
-	}
+	private static final int SIZEOF_INT = 4;
 
 	/**
 	 * Create a new header.  Note that the bytes in the header cannot be changed
-	 * @param headerBytes the characters to include in teh header.  This must be less than 4
+	 * @param identifier the characters to include in teh header.  This must be less than 4
+	 * @param length the length of the header
 	 */
-	Header(char[] headerBytes)
+	Header(char identifier, int length)
 	{
-		//Check to make sure the header isn't more than 4 bytes
-		if (headerBytes.length > 4)
-			throw new IllegalArgumentException();
 
-		byte[] tempBytes = new byte[8];
-
-		for (int i = 0; i < 4; i++)
-			tempBytes[i] = (byte) headerBytes[i];
-		for (int i = 4; i < 8; i++)
-			tempBytes[i] = 0;
-
-		this.header = tempBytes;
 	}
 
 	/**
@@ -57,4 +33,44 @@ class Header {
 	{
 		return header;
 	}
+
+	/**
+	 * Convert an integer into an array of 4 bytes
+	 * Note that this function will ignore the sign bit
+	 * The array of bytes is encoded in Little Endian IE The least significant byte is first
+	 * @param n the integer to convert
+	 * @return an array of 4 bytes containing the value of the integer
+	 */
+	public static byte[] intToBytes(int n)
+	{
+		byte[] bytes = new byte[4];
+
+		for(int i = 3; i >= 0; i--)
+		{
+			bytes[i] = (byte) (n / (int) Math.pow(256, i));
+		}
+
+		return bytes;
+	}
+
+	/**
+	 * Convert an array of bytes to a corresponding {@code int}
+	 * This function expects the bytes to be arranged in Little Endian IE The least significant byte should be in {@code n[0]}
+	 * @param n the {@code byte} array to convert
+	 * @return an {@code int} of the same value as 4 bytes
+	 */
+	public static int bytesToInt(byte[] n)
+	{
+		if(n.length > Header.SIZEOF_INT)
+			throw new IllegalArgumentException("Cannot store more than 4 bytes in integer");
+		int total = 0;
+
+		for(int i = 0; i < 4; i++)
+		{
+			total += ((int) n[i]) * Math.pow(256, i);
+		}
+
+		return total;
+	}
+
 }
